3555a4077d51200aa92c5f42b3d5912e49ce1447
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-07-14 17:53:10 -0700

    Set up page layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,80 @@
 import './style.css';
+import API_KEY from './apikey';
+
+const temperatureElem = document.querySelector('.temperature');
+const feelsLikeElem = document.querySelector('.feels-like');
+const tempLowHighElem = document.querySelector('.temp-low-high');
+const humidityElem = document.querySelector('.humidity');
+const weatherIconElem = document.querySelector('.weather-icon');
+const weatherMainElem = document.querySelector('.weather-main');
+const weatherDescElem = document.querySelector('.weather-description');
+
+function getWeatherIcon(iconCode) {
+  let weatherIcon;
+  switch (iconCode) {
+    case '01d':
+    case '01n':
+      weatherIcon = '●';
+      break;
+    case '02d':
+    case '02n':
+      weatherIcon = '🌤';
+      break;
+    case '03d':
+    case '03n':
+      weatherIcon = '☁';
+      break;
+    case '04d':
+    case '04n':
+      weatherIcon = '☁☁';
+      break;
+    case '09d':
+    case '09n':
+      weatherIcon = '🌧';
+      break;
+    case '10d':
+    case '10n':
+      weatherIcon = '🌧🌧';
+      break;
+    case '11d':
+    case '11n':
+      weatherIcon = '⛈';
+      break;
+    case '13d':
+    case '13n':
+      weatherIcon = '❄';
+      break;
+    case '50d':
+    case '50n':
+      weatherIcon = '🌫';
+      break;
+    default:
+      weatherIcon = '?';
+      break;
+  }
+  return weatherIcon;
+}
+
+function updateAllElements(weatherData) {
+  console.log(weatherData);
+  temperatureElem.textContent = `${Math.round(weatherData.main.temp)}°F`;
+  feelsLikeElem.textContent = `Feels like ${Math.round(weatherData.main.feels_like)}°F`;
+  tempLowHighElem.textContent = `${Math.round(weatherData.main.temp_max)}°F / ${Math.round(weatherData.main.temp_min)}°F`;
+  humidityElem.textContent = `Humidity: ${weatherData.main.humidity}%`;
+  weatherIconElem.textContent = getWeatherIcon(weatherData.weather[0].icon);
+  weatherMainElem.textContent = `${weatherData.weather[0].main}`;
+  weatherDescElem.textContent = `${weatherData.weather[0].description}`;
+}
+
+function fetchWeatherFromCity(lat, lon, units) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${API_KEY}`, { mode: 'cors' })
+    .then((response) => response.json())
+    .then((response) => {
+      updateAllElements(response);
+    })
+    .catch((err) => {
+      console.log(`Error! ${err}`);
+    });
+}
+
+fetchWeatherFromCity('51.50', '0.13', 'imperial');
